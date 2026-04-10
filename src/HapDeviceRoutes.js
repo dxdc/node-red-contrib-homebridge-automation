bab1574673f5e6e5afabbe1dc85e3193baa98dcb
Daniel Caspi <dan@element26.net>
2026-04-10 14:35:33 -0500

    refactor: simplify route registration in HapDeviceRoutes

--- src/HapDeviceRoutes.js
+++ src/HapDeviceRoutes.js
@@ -6,10 +6,16 @@ class HapDeviceRoutes {
   }
 
   // POST /hap-device/refresh/:id
-  refreshDevice(req, res) {
+  async refreshDevice(req, res) {
     const conf = this.RED.nodes.getNode(req.params.id);
     if (conf) {
-      res.status(200).send();
+      try {
+        await conf.refreshDeviceList();
+        res.status(200).send();
+      } catch (e) {
+        debug('Error refreshing devices: %s', e.message);
+        res.status(500).send({ error: e.message });
+      }
     } else {
       debug("Can't refresh until deployed");
       res.status(404).send();
@@ -17,12 +23,12 @@ class HapDeviceRoutes {
   }
 
   // GET /hap-device/evDevices/
-  getDevices(req, res, perms) {
-    const devices = this.RED.nodes.getNode(req.params.id)?.evDevices;
+  getDevices(req, res, key) {
+    const devices = this.RED.nodes.getNode(req.params.id)?.[key];
     if (devices && devices.length) {
       res.send(devices);
     } else {
-      res.status(404).send({ error: `No devices found for perms: ${perms}` });
+      res.status(404).send({ error: `No devices found for ${key}` });
     }
   }
 
@@ -39,19 +45,18 @@ class HapDeviceRoutes {
 
   // Register all routes
   registerRoutes() {
-    const routes = [
-      { method: 'post', path: '/hap-device/refresh/:id', permission: 'hb-event.read', handler: this.refreshDevice },
-      { method: 'get', path: '/hap-device/evDevices/', permission: 'hb-event.read', handler: (req, res) => this.getDevices(req, res, 'ev') },
-      { method: 'get', path: '/hap-device/evDevices/:id', permission: 'hb-event.read', handler: (req, res) => this.getDeviceById(req, res, 'evDevices') },
-      { method: 'post', path: '/hap-device/refresh/:id', permission: 'hb-resume.read', handler: this.refreshDevice },
-      { method: 'get', path: '/hap-device/evDevices/', permission: 'hb-resume.read', handler: (req, res) => this.getDevices(req, res, 'ev') },
-      { method: 'get', path: '/hap-device/evDevices/:id', permission: 'hb-resume.read', handler: (req, res) => this.getDeviceById(req, res, 'evDevices') },
-      { method: 'get', path: '/hap-device/ctDevices/', permission: 'hb-control.read', handler: (req, res) => this.getDevices(req, res, 'pw') },
-      { method: 'get', path: '/hap-device/ctDevices/:id', permission: 'hb-control.read', handler: (req, res) => this.getDeviceById(req, res, 'ctDevices') },
+    const nodeTypes = [
+      { permission: 'hb-event.read', deviceKey: 'evDevices' },
+      { permission: 'hb-resume.read', deviceKey: 'evDevices' },
+      { permission: 'hb-status.read', deviceKey: 'evDevices' },
+      { permission: 'hb-control.read', deviceKey: 'ctDevices' },
     ];
 
-    routes.forEach(({ method, path, permission, handler }) => {
-      this.RED.httpAdmin[method](path, this.RED.auth.needsPermission(permission), handler.bind(this));
+    nodeTypes.forEach(({ permission, deviceKey }) => {
+      const auth = this.RED.auth.needsPermission(permission);
+      this.RED.httpAdmin.post('/hap-device/refresh/:id', auth, this.refreshDevice.bind(this));
+      this.RED.httpAdmin.get(`/hap-device/${deviceKey}/`, auth, (req, res) => this.getDevices(req, res, deviceKey));
+      this.RED.httpAdmin.get(`/hap-device/${deviceKey}/:id`, auth, (req, res) => this.getDeviceById(req, res, deviceKey));
     });
   }
 }
